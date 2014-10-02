e242062e7e82a38f57a4a93047764c136b0adde9
Mihail-K <Mihail.K@fimu.tk>
2014-10-02 00:17:23 -0400

    Cleaned up redundant code in defines.

--- Defines.h
+++ Defines.h
@@ -1,16 +1,13 @@
 # ifndef __DEFINES_H__
 # define __DEFINES_H__
 
-# ifdef __cplusplus
-#	define inline inline
-# else
+# ifndef __cplusplus
 #	if defined __GNUC__
 #		define inline __inline__
 #	elif defined _MSC_VER
 #		define inline __inline
 #	else
 		/* Just hope for the best */
-#		define inline inline
 #	endif /* Compiler */
 # endif /* C++ */
 
