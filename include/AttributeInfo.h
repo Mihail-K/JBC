d3deea9138ce5a63deb17a4b11d06adbcbc41231
Mihail.K <Mihail.K@fimu.tk>
2014-11-29 13:20:16 -0500

    Added flag manipulation to Member info.

--- include/AttributeInfo.h
+++ include/AttributeInfo.h
@@ -13,35 +13,6 @@
 
 namespace JBC {
 
-/* Access Flags */
-
-enum FieldAccessFlags {
-	ACC_PUBLIC		= 0x0001,
-	ACC_PRIVATE		= 0x0002,
-	ACC_PROTECTED	= 0x0004,
-	ACC_STATIC		= 0x0008,
-	ACC_FINAL		= 0x0010,
-	ACC_VOLATILE	= 0x0040,
-	ACC_TRANSIENT	= 0x0080,
-	ACC_SYNTHETIC	= 0x1000,
-	ACC_ENUM		= 0x4000
-};
-
-enum MethodAccessFlags {
-//	ACC_PUBLIC		= 0x0001,
-//	ACC_PRIVATE		= 0x0002,
-//	ACC_PROTECTED	= 0x0004,
-//	ACC_STATIC		= 0x0008,
-//	ACC_FINAL		= 0x0010,
-	ACC_SYNCHRONIZED= 0x0020,
-	ACC_BRIDGE		= 0x0040,
-	ACC_VARARGS		= 0x0080,
-	ACC_NATIVE		= 0x0100,
-	ACC_ABSTRACT	= 0x0400,
-	ACC_STRICT		= 0x0800,
-//	ACC_SYNTHETIC	= 0x1000
-};
-
 /* Attribute Info */
 
 struct AttributeInfo {
--- include/MemberInfo.h
+++ include/MemberInfo.h
@@ -27,6 +27,33 @@ namespace JBC {
 class ClassFile;
 struct AttributeInfo;
 
+/* Access Flags */
+
+enum MemberFlags {
+	FIELD_PUBLIC		= 0x0001,
+	FIELD_PRIVATE		= 0x0002,
+	FIELD_PROTECTED		= 0x0004,
+	FIELD_STATIC		= 0x0008,
+	FIELD_FINAL			= 0x0010,
+	FIELD_VOLATILE		= 0x0040,
+	FIELD_TRANSIENT		= 0x0080,
+	FIELD_SYNTHETIC		= 0x1000,
+	FIELD_ENUM			= 0x4000,
+
+	METHOD_PUBLIC		= 0x0001,
+	METHOD_PRIVATE		= 0x0002,
+	METHOD_PROTECTED	= 0x0004,
+	METHOD_STATIC		= 0x0008,
+	METHOD_FINAL		= 0x0010,
+	METHOD_SYNCHRONIZED	= 0x0020,
+	METHOD_BRIDGE		= 0x0040,
+	METHOD_VARARGS		= 0x0080,
+	METHOD_NATIVE		= 0x0100,
+	METHOD_ABSTRACT		= 0x0400,
+	METHOD_STRICT		= 0x0800,
+	METHOD_SYNTHETIC	= 0x1000
+};
+
 /* Field/Method Info */
 
 /**
@@ -73,6 +100,34 @@ public:
 	~MemberInfo();
 
 public:
+	/**
+	 * @breif Returns a reference to this member's flags.
+	 **/
+	inline
+	uint16_t &Flags() {
+		return access_flags;
+	}
+
+	/**
+	 * @breif Returns the state of a single flag.
+	 *
+	 * @param flag The flag to check for.
+	 **/
+	bool GetFlag(MemberFlags flag) {
+		return !!(access_flags & flag);
+	}
+
+	/**
+	 * @brief Sets the state of a single flag.
+	 *
+	 * @param flag The flag to be modified.
+	 * @param state The new state for the flag.
+	 **/
+	void SetFlag(MemberFlags flag, bool state) {
+		if(state) access_flags |= flag;
+		else access_flags &= ~flag;
+	}
+
 	/**
 	 * @brief Returns this member's name.
 	 *
